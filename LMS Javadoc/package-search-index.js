05627478e2e56b92e09df48fcd85327d544ed86d
Erik <78745844+erikgraybill@users.noreply.github.com>
2023-02-15 19:04:27 -0500

    Add files via upload

---
+++ LMS Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Authentication"},{"l":"Authentication.Model"},{"l":"Course"},{"l":"Course.Model"},{"l":"Message"},{"l":"Message.Model"},{"l":"User"},{"l":"User.Model"},{"l":"User.Model.Student"},{"l":"User.Model.TeachingTeam"}];updateSearchResults();
