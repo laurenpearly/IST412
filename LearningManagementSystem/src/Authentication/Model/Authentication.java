a588b5376845521ba014f290d00a9ec37a698822
zmh5199 <zmh5199@psu.edu>
2023-03-05 15:17:19 -0500

    Authentication Constructor and imports
    Added the Authentication.Model.Authentication constructor and the necessary imports

--- LearningManagementSystem/src/Authentication/Model/Authentication.java
+++ LearningManagementSystem/src/Authentication/Model/Authentication.java
@@ -1,16 +1,38 @@
 package Authentication.Model;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Authentication {
-    ArrayList<String> users;
+    Scanner sc = new Scanner("userInfo.txt");
+    ArrayList<String> users = new ArrayList<String>();
+    String userName;
+    String userPassword;
 
     /**
      * Constructs Authentication.
      * Builds the user ArrayList from a txt file for use in program.
      */
     public Authentication() {
+        try{
+        File userInfo = new File("D:\\IST412\\LearningManagementSystem\\src\\Authentication\\userInfo.txt");   
+        Scanner sc = new Scanner(userInfo);
+        
+        while (sc.hasNextLine()){
+            String accountInfo = sc.nextLine();
+            String[] str = new String[2];
+            str = accountInfo.split(", ");
 
+            users.add(str[0]);  // userName (even numbers)
+            users.add(str[1]);  // userPassword (odd numbers)
+
+            str = null;         // Empties temp list
+        }
+        sc.close();
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
     }
 
     /**
